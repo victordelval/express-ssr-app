e7c38611b3235c4eb410bab9d231bbd977cc129d
victordelval <victor.val.andres@gmail.com>
2018-03-10 01:20:48 +0100

    Adds custom settings

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import express from 'express'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
 
 
 const app = express()
 
+// custom settings
+
+app.disable('x-powered-by')
+app.set('env', 'development')  // process.env ...
+
+app.use(morgan())
+// app.use(morgan('tiny'))  // 'combined'
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+// root url
 app.get('/', (req, res) => {
     res.end('Hola Mundo!')
 })
