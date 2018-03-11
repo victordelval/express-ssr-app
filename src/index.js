f694ccaf6e5457ed09baed532efd1eba50c8528c
victordelval <victor.val.andres@gmail.com>
2018-03-11 09:25:03 +0100

    Adds another route with navigation betwwen them

--- src/index.js
+++ src/index.js
@@ -20,15 +20,29 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-// root url
+// Routes
 app.get('/', (req, res) => {
     // res.end('Hola Mundo!')
-    res.render('home', {
-        title: 'Curso de OpenWebinars',
-        message: 'Nuestro primer layout con variables'
-    })
+    // res.render('home', {
+    //     title: 'Curso de OpenWebinars',
+    //     message: 'Nuestro primer layout con variables'
+    // })
+    res.write(`
+        <h1>Curso OpenWebinars</h1>
+        <a href="/temario">Temario</a>
+    `)
+    res.end();
 })
 
+app.get('/temario', (req, res) => {
+    res.write(`
+        <h1>Temario del curso de NodeJS</h1>
+        <a href="/">Volver</a>
+    `)
+    res.end();
+})
+
+// server listen
 app.listen('9000', () => {
     console.log('Servidor arrancado en http://localhost:9000')
 })
