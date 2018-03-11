87dfd36f97f2d3564e93f305781d07daf85933f0
victordelval <victor.val.andres@gmail.com>
2018-03-11 11:37:35 +0100

    Improves templates adding some partials for reuse

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import path from 'path'
 import express from 'express'
-import morgan from 'morgan'
+import logger from 'morgan'
 import bodyParser from 'body-parser'
 
 
@@ -11,9 +11,7 @@ const app = express()
 app.disable('x-powered-by')
 app.set('env', 'development')  // process.env ...
 
-app.use(morgan())
-// app.use(morgan('tiny'))  // 'combined'
-
+app.use(logger('combined'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -23,22 +21,26 @@ app.set('view engine', 'pug')
 // Routes
 app.get('/', (req, res) => {
     // res.end('Hola Mundo!')
-    // res.render('home', {
-    //     title: 'Curso de OpenWebinars',
-    //     message: 'Nuestro primer layout con variables'
-    // })
-    res.write(`
-        <h1>Curso OpenWebinars</h1>
-        <a href="/temario">Temario</a>
-    `)
+    res.render('home', {
+        title: 'OpenWebinars',
+        message: 'Curso de NodeJS - Home'
+    })
+    // res.write(`
+    //     <h1>Curso OpenWebinars</h1>
+    //     <a href="/temario">Temario</a>
+    // `)
     res.end();
 })
 
 app.get('/temario', (req, res) => {
-    res.write(`
-        <h1>Temario del curso de NodeJS</h1>
-        <a href="/">Volver</a>
-    `)
+    // res.write(`
+    //     <h1>Temario del curso de NodeJS</h1>
+    //     <a href="/">Volver</a>
+    // `)
+    res.render('temario', {
+        title: 'OpenWebinars',
+        message: 'Temario del curso de NodeJS'
+    })
     res.end();
 })
 
